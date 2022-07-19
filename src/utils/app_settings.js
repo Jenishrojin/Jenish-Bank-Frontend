4a536c435f4069bb2311a739787c68d5325e95a3
Jenishrojin <jenishrojin2002@gmail.com>
2022-07-19 13:36:52 +0530

    Changed Base Url

--- src/utils/app_settings.js
+++ src/utils/app_settings.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://localhost:8080"
+// export const baseUrl = "http://localhost:8080"
 
-// export const baseUrl = "http://localhost:8080/"
+export const baseUrl = "https://jenish-bank.herokuapp.com"
